028799034c6445fdabfc88c0514e48f4b58b8b20
Ajaysvasan <ajay192006@gmail.com>
2025-04-17 23:04:53 +0530

    some minor updates

--- src/start.bat
+++ src/start.bat
@@ -5,7 +5,7 @@ set fileThree=dbms
 
 echo Compiling %fileOne%.cpp , %fileTwo%.cpp and %fileThree%.cpp...
 
-g++ %fileOne%.cpp %fileTwo%.cpp %fileThree%.cpp -o %fileOne%.exe
+g++ %fileOne%.cpp %fileTwo%.cpp %fileThree%.cpp -o MiniDBMS.exe
 
 if %errorlevel% neq 0 (
     echo Compilation failed!
@@ -13,6 +13,6 @@ if %errorlevel% neq 0 (
     exit /b %errorlevel%
 )
 
-echo Running %fileOne%.exe...
-%fileOne%.exe
+echo Running MiniDBMS.exe...
+MiniDBMS.exe
 pause
